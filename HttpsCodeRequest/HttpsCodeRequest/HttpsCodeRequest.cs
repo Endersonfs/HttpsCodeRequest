b7edd8de68acf2c12d06b6c99449e9e044e3aa4d
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2023-05-30 15:09:57 -0400

    Agregando metodos y clase para tener la descripcion

---
+++ HttpsCodeRequest/HttpsCodeRequest/HttpsCodeRequest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpsCodeRequest
+{
+    public enum HttpStatusCode
+    {
+        [Description("The server has received the request headers and the client should proceed to send the request body (in the case of a request for which a body needs to be sent; for example, a POST request). Sending a large request body to a server after a request has been rejected for inappropriate headers would be inefficient. To have a server check the request's headers, a client must send Expect: 100-continue as a header in its initial request and receive a 100 Continue status code in response before sending the body. If the client receives an error code such as 403 (Forbidden) or 405 (Method Not Allowed) then it should not send the request's body. The response 417 Expectation Failed indicates that the request should be repeated without the Expect header as it indicates that the server does not support expectations (this is the case, for example, of HTTP/1.0 servers).")]
+        Continue = 100,
+        SwitchingProtocols = 101,
+        Processing = 102,
+        EarlyHints = 103,
+        [Description("Hola codigo 200")]
+        OK = 200,
+        Created = 201,
+        Accepted = 202,
+        NonAuthoritativeInformation = 203,
+        NoContent = 204,
+        ResetContent = 205,
+        PartialContent = 206,
+        MultiStatus = 207,
+        AlreadyReported = 208,
+        IMUsed = 226,
+        Ambiguous = 300,
+        MultipleChoices = 300,
+        Moved = 301,
+        MovedPermanently = 301,
+        Found = 302,
+        Redirect = 302,
+        RedirectMethod = 303,
+        SeeOther = 303,
+        NotModified = 304,
+        UseProxy = 305,
+        Unused = 306,
+        RedirectKeepVerb = 307,
+        TemporaryRedirect = 307,
+        PermanentRedirect = 308,
+        BadRequest = 400,
+        Unauthorized = 401,
+        PaymentRequired = 402,
+        Forbidden = 403,
+        NotFound = 404,
+        MethodNotAllowed = 405,
+        NotAcceptable = 406,
+        ProxyAuthenticationRequired = 407,
+        RequestTimeout = 408,
+        Conflict = 409,
+        Gone = 410,
+        LengthRequired = 411,
+        PreconditionFailed = 412,
+        RequestEntityTooLarge = 413,
+        RequestUriTooLong = 414,
+        UnsupportedMediaType = 415,
+        RequestedRangeNotSatisfiable = 416,
+        ExpectationFailed = 417,
+        MisdirectedRequest = 421,
+        UnprocessableEntity = 422,
+        Locked = 423,
+        FailedDependency = 424,
+        UpgradeRequired = 426,
+        PreconditionRequired = 428,
+        TooManyRequests = 429,
+        RequestHeaderFieldsTooLarge = 431,
+        UnavailableForLegalReasons = 451,
+        InternalServerError = 500,
+        NotImplemented = 501,
+        BadGateway = 502,
+        ServiceUnavailable = 503,
+        GatewayTimeout = 504,
+        HttpVersionNotSupported = 505,
+        VariantAlsoNegotiates = 506,
+        InsufficientStorage = 507,
+        LoopDetected = 508,
+        NotExtended = 510,
+        NetworkAuthenticationRequired = 511
+    }
+    public static class HttpsCode 
+    {
+        /// <summary>
+        /// Metodo para tener la descripcion de los HttpsCode.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescription(this Enum value) 
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attr = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attr.Length == 0 ? value.ToString() : (attr[0] as DescriptionAttribute).Description;
+        }
+
+        // <summary>
+        /// Metodo para tener la descripcion de los HttpsCode, este metedo recibe un codigo para validar el mismo y retornar la descripcion.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string GetDescriptionWithCode(Enum code)
+        {
+            var field = code.GetType().GetField(code.ToString());
+            var attr = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attr.Length == 0 ? code.ToString() : (attr[0] as DescriptionAttribute).Description;
+        }
+    }
+}
