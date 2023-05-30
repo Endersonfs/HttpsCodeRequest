a2f20d8b8d10beb069f4ef6e632087fff94e2b7f
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2023-05-30 16:34:27 -0400

    Agregando metodo y publicando en nuget

---
+++ ProbandoNugetHttpsCode/ProbandoNugetHttpsCode/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HttpsCodeRequest;
+
+var soto = HttpsCode.OK.GetDescription();
+
+Console.WriteLine(HttpsCode.OK.GetDescription());
