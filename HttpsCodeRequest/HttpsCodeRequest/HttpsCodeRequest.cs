bdbde6c6e69f687e2c81900127c7c49d078f72c1
Enderson Florian <eflorian@phoenixcalibrationdr.com>
2023-05-31 15:29:45 -0400

    Capturarando error para el metodo GetDescriptionByCode

--- HttpsCodeRequest/HttpsCodeRequest/HttpsCodeRequest.cs
+++ HttpsCodeRequest/HttpsCodeRequest/HttpsCodeRequest.cs
@@ -81,7 +81,7 @@ namespace HttpsCodeRequest
         #region Client errors Responses
         [Description("The 400 (Bad Request) status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).\r\n\r\n")]
         BadRequest = 400,
-        [Description("Similar to 403 Forbidden, but specifically for use when authentication is required and has failed or has not yet been provided. The response must include a WWW-Authenticate header field containing a challenge applicable to the requested resource. See Basic access authentication and Digest access authentication. 401 semantically means \"unauthorised\", the user does not have valid authentication credentials for the target resource.\r\nSome sites incorrectly issue HTTP 401 when an IP address is banned from the website (usually the website domain) and that specific address is refused permission to access a website.")]
+        [Description("Similar to 403 Forbidden, but specifically for use when authentication is required and has failed or has not yet been provided. The response must include a WWW-Authenticate header field containing a challenge applicable to the requested resource. See Basic access authentication and Digest access authentication. 401 semantically means unauthorised, the user does not have valid authentication credentials for the target resource.\r\nSome sites incorrectly issue HTTP 401 when an IP address is banned from the website (usually the website domain) and that specific address is refused permission to access a website.")]
         Unauthorized = 401,
         [Description("Reserved for future use. The original intention was that this code might be used as part of some form of digital cash or micropayment scheme, as proposed, for example, by GNU Taler,[14] but that has not yet happened, and this code is not widely used. Google Developers API uses this status if a particular developer has exceeded the daily limit on requests.[15] Sipgate uses this code if an account does not have sufficient funds to start a call.[16] Shopify uses this code when the store has not paid their fees and is temporarily disabled.[17] Stripe uses this code for failed payments where parameters were correct, for example blocked fraudulent payments")]
         PaymentRequired = 402,
@@ -95,67 +95,69 @@ namespace HttpsCodeRequest
         NotAcceptable = 406,
         [Description("The client must first authenticate itself with the proxy.")]
         ProxyAuthenticationRequired = 407,
-        [Description("The server timed out waiting for the request. According to HTTP specifications: \"The client did not produce a request within the time that the server was prepared to wait. The client MAY repeat the request without modifications at any later time.\"")]
+        [Description("The server timed out waiting for the request. According to HTTP specifications: The client did not produce a request within the time that the server was prepared to wait. The client MAY repeat the request without modifications at any later time.")]
         RequestTimeout = 408,
         [Description("Indicates that the request could not be processed because of conflict in the current state of the resource, such as an edit conflict between multiple simultaneous updates.")]
         Conflict = 409,
-        [Description("Indicates that the resource requested was previously in use but is no longer available and will not be available again. This should be used when a resource has been intentionally removed and the resource should be purged. Upon receiving a 410 status code, the client should not request the resource in the future. Clients such as search engines should remove the resource from their indices. Most use cases do not require clients and search engines to purge the resource, and a \"404 Not Found\" may be used instead.")]
+        [Description("Indicates that the resource requested was previously in use but is no longer available and will not be available again. This should be used when a resource has been intentionally removed and the resource should be purged. Upon receiving a 410 status code, the client should not request the resource in the future. Clients such as search engines should remove the resource from their indices. Most use cases do not require clients and search engines to purge the resource, and a 404 Not Found may be used instead.")]
         Gone = 410,
         [Description("The request did not specify the length of its content, which is required by the requested resource.")]
         LengthRequired = 411,
         [Description("The server does not meet one of the preconditions that the requester put on the request header fields.")]
         PreconditionFailed = 412,
-        [Description("The request is larger than the server is willing or able to process. Previously called \"Request Entity Too Large\" in RFC 2616.")]
+        [Description("The request is larger than the server is willing or able to process. Previously called Request Entity Too Large in RFC 2616.")]
         RequestEntityTooLarge = 413,
-        [Description("The URI provided was too long for the server to process. Often the result of too much data being encoded as a query-string of a GET request, in which case it should be converted to a POST request. Called \"Request-URI Too Long\" previously in RFC 2616.")]
+        [Description("The URI provided was too long for the server to process. Often the result of too much data being encoded as a query-string of a GET request, in which case it should be converted to a POST request. Called Request-URI Too Long previously in RFC 2616.")]
         RequestUriTooLong = 414,
         [Description("The request entity has a media type which the server or resource does not support. For example, the client uploads an image as image/svg+xml, but the server requires that images use a different format.")]
         UnsupportedMediaType = 415,
-        [Description("The client has asked for a portion of the file (byte serving), but the server cannot supply that portion. For example, if the client asked for a part of the file that lies beyond the end of the file. Called \"Requested Range Not Satisfiable\" previously RFC 2616")]
+        [Description("The client has asked for a portion of the file (byte serving), but the server cannot supply that portion. For example, if the client asked for a part of the file that lies beyond the end of the file. Called Requested Range Not Satisfiable previously RFC 2616")]
         RequestedRangeNotSatisfiable = 416,
         [Description("The server cannot meet the requirements of the Expect request-header field.")]
         ExpectationFailed = 417,
-        [Description("")]
+        [Description("The request was directed at a server that is not able to produce a response (for example because of connection reuse).")]
         MisdirectedRequest = 421,
-        [Description("")]
+        [Description("The request was well-formed but was unable to be followed due to semantic errors.")]
         UnprocessableEntity = 422,
-        [Description("")]
+        [Description("The resource that is being accessed is locked.")]
         Locked = 423,
-        [Description("")]
+        [Description("The request failed because it depended on another request and that request failed (e.g., a PROPPATCH).")]
         FailedDependency = 424,
-        [Description("")]
+        [Description("The request failed because it depended on another request and that request failed (e.g., a PROPPATCH).")]
+        TooEarly = 425,
+        [Description("Indicates that the server is unwilling to risk processing a request that might be replayed.")]
         UpgradeRequired = 426,
-        [Description("")]
+        [Description("The origin server requires the request to be conditional. Intended to prevent the 'lost update' problem, where a client GETs a resource's state, modifies it, and PUTs it back to the server, when meanwhile a third party has modified the state on the server, leading to a conflict")]
         PreconditionRequired = 428,
-        [Description("")]
+        [Description("The user has sent too many requests in a given amount of time. Intended for use with rate-limiting schemes")]
         TooManyRequests = 429,
-        [Description("")]
+        [Description("The server is unwilling to process the request because either an individual header field, or all the header fields collectively, are too large.")]
         RequestHeaderFieldsTooLarge = 431,
-        [Description("")]
+        [Description("A server operator has received a legal demand to deny access to a resource or to a set of resources that includes the requested resource.")]
         UnavailableForLegalReasons = 451,
         #endregion
         #region Server errors Responses
-        [Description("")]
+        [Description("A generic error message, given when an unexpected condition was encountered and no more specific message is suitable.")]
         InternalServerError = 500,
-        [Description("")]
+        [Description("The server either does not recognize the request method, or it lacks the ability to fulfil the request. Usually this implies future availability (e.g., a new feature of a web-service API)")]
         NotImplemented = 501,
-        [Description("")]
+        [Description("The server was acting as a gateway or proxy and received an invalid response from the upstream server.")]
         BadGateway = 502,
-        [Description("")]
+        [Description("The server cannot handle the request (because it is overloaded or down for maintenance). Generally, this is a temporary state.")]
         ServiceUnavailable = 503,
-        [Description("")]
+        [Description("The server was acting as a gateway or proxy and did not receive a timely response from the upstream server.")]
         GatewayTimeout = 504,
-        [Description("")]
+        [Description("The server does not support the HTTP version used in the request.")]
         HttpVersionNotSupported = 505,
-        [Description("")]
+        [Description("Transparent content negotiation for the request results in a circular reference.")]
         VariantAlsoNegotiates = 506,
-        [Description("")]
+        [Description("The server is unable to store the representation needed to complete the request.")]
         InsufficientStorage = 507,
-        [Description("")]
+        [Description("The server detected an infinite loop while processing the request (sent instead of 208 Already Reported).")]
         LoopDetected = 508,
-        [Description("")]
+        [Description("Further extensions to the request are required for the server to fulfil it.")]
         NotExtended = 510,
-        [Description("")]
+        [Description("he client needs to authenticate to gain network access. Intended for use by intercepting proxies used to control access to the network (e.g., captive portals used to require agreement to Terms of Service before granting full Internet access via a Wi-Fi hotspot).")]
         NetworkAuthenticationRequired = 511
         #endregion
     }
@@ -183,9 +185,16 @@ namespace HttpsCodeRequest
         /// <returns></returns>
         public static string GetDescriptionByCode(Enum code)
         {
-            var field = code.GetType().GetField(code.ToString());
-            var attr = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
-            return attr.Length == 0 ? code.ToString() : (attr[0] as DescriptionAttribute).Description;
+            try
+            {
+                var field = code.GetType().GetField(code.ToString());
+                var attr = field.GetCustomAttributes(typeof(DescriptionAttribute), false);
+                return attr.Length == 0 ? code.ToString() : (attr[0] as DescriptionAttribute).Description;
+            }
+            catch(Exception ex) 
+            {
+                return ex.Message;
+            }
         }
     }
 }
